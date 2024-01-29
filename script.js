ed7900ceb66e8e89d1767574f81cddd15c4cc88f
amandakenes <amandakeness@outlook.com>
2024-01-29 14:02:09 -0300

    finish go fight function

--- script.js
+++ script.js
@@ -168,7 +168,11 @@ function fightDragon() {
 }
 
 function goFight() {
-    update(locations[3]);
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName = monsters[fighting].name;
+  monsterHealthText = monsters[fighting].health;
 }
 
 function attack() {}
